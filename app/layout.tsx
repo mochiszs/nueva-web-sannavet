e0d5a8275a920d21b85b8d48e4b1618c37e7eae7
mochiszs <ruaperezmoises@gmail.com>
2025-04-04 18:04:34 -0500

    ahora si definitivo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import Header from "@/components/header"
 import Footer from "@/components/footer"
 
 export const metadata: Metadata = {
-  title: "Sannavet - Cuidamos la salud de tu mascota",
+  title: "Clínica Veterinaria",
   description: "Cuidamos de la salud y bienestar de tus mascotas",
-  icons: "/nuevasfotos/ISOTIPO SANNAVET_Mesa de trabajo 1.png", // Asegúrate de que esta ruta sea correcta
 }
 
 export default function RootLayout({
@@ -17,18 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <head>
-        {/* Meta etiquetas, íconos, etc. */}
-        <meta name="description" content="Cuidamos de la salud y bienestar de tus mascotas"/>
-        <link rel="icon" href="/nuevasfotos/ISOTIPO SANNAVET_Mesa de trabajo 1.png"/> {/* Aquí se define el ícono */}
-      </head>
       <body>
         <Header />
-        {children} {/* Esto asegura que el contenido de la página se inserte aquí */}
+        {children}
         <Footer />
       </body>
     </html>
   )
 }
 
-import './globals.css'
